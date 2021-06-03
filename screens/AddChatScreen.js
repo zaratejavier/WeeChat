5d13893856bf544a35de2ab255127107b0a6dfdd
Javier Zarate <zarate.javier.jz@gmail.com>
2021-06-02 18:26:23 -0600

    added input and styled chat screen

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -1,14 +1,50 @@
-import React from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Button, Input } from 'react-native-elements'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { db } from '../firebase'
 
-const AddChatScreen = ({navigation}) => {
+const AddChatScreen = ({ navigation }) => {
+  const [input, setInput] = useState("");
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Add new chat",
+      headerBackTitle: "Chats",
+    })
+  }, [])
+
+  const createChat = async () => {
+    await db.collection('chats').add({
+      chatName: input
+    }).then(() => {
+      navigation.goBack()
+    }).catch((error) => alert(error))
+
+  }
+
   return (
-    <View>
-      <Text>Add Chat Screen</Text>
+    <View style={styles.container}>
+      <Input
+        placeholder="Enter a chat name"
+        value={input}
+        onChangeText={(text) => setInput(text)}
+        onSubmitEditing={createChat}
+        leftIcon={
+          <Icon name='wechat' type='antDesign' size={24} color="black"/>
+        }
+      />
+      <Button onPress={createChat} title='Create new chat'/>
     </View>
   )
 }
 
 export default AddChatScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 30,
+    height: "100%"
+  }
+})
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { Avatar } from 'react-native-elements';
 import CustomListItem from '../components/CustomListItem';
 import { auth, db } from '../firebase';
-import { AntDesign, SimpleLineIcons } from "@expo/vector-icons";
+import { AntDesign, SimpleLineIcons } from '@expo/vector-icons';
 
 const HomeScreen = ({ navigation }) => {
   const signOutUser = () => {
@@ -24,6 +24,10 @@ const HomeScreen = ({ navigation }) => {
         <View style={{ marginLeft: 20 }}>
           <TouchableOpacity onPress={signOutUser} activeOpacity={0.5}>
             {/* <Text>
+            {auth?.currentUser?.displayName}
+
+            </Text> */}
+            {/* <Text>
               Sign out
               </Text> */}
             <Avatar
@@ -36,19 +40,23 @@ const HomeScreen = ({ navigation }) => {
         </View>
       ),
       headerRight: () => (
-        <View style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          width: 80,
-          marginRight: 20,
-        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            width: 80,
+            marginRight: 20
+          }}
+        >
           <TouchableOpacity activeOpacity={0.5}>
-            <AntDesign name='camerao' size={24} color='black'/>
-
+            <AntDesign name="camerao" size={24} color="black" />
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => navigation.navigate('AddChat')} activeOpacity={0.5}>
-          <SimpleLineIcons name="pencil" size={24} color="black"/>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('AddChat')}
+            activeOpacity={0.5}
+          >
+            <SimpleLineIcons name="pencil" size={24} color="black" />
           </TouchableOpacity>
         </View>
       )
